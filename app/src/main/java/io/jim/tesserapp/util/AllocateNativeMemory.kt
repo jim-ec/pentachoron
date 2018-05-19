144c9ea5ced67209d739cab7cc943d4ae16e9be9
Jim Eckerlein <jim.eckerlein@gmail.com>
2018-05-19 11:14:16 +0200

    0.0.0-replace-float-with-double.1+4 Fix doc for allocateNativeFloatMemory()
    Point out that the function creates a ByteBuffer, although it's name sounds like it does create a FloatBuffer instead.

--- app/src/main/java/io/jim/tesserapp/util/AllocateNativeMemory.kt
+++ app/src/main/java/io/jim/tesserapp/util/AllocateNativeMemory.kt
@@ -15,6 +15,8 @@ fun allocateNativeMemory(bytes: Int) =
 
 /**
  * Returns natively allocated memory, considering the native byte endianness.
+ * The generated buffer is still a [ByteBuffer], only it's memory size is always a multiply
+ * of a float's memory size.
  * @param floats Size of memory to be allocated, in floats.
  */
 fun allocateNativeFloatMemory(floats: Int) =
