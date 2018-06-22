2ef8c48088baaad64b2a4dfa29b779d607801060
Jim Eckerlein <jim.eckerlein@gmail.com>
2018-06-22 13:17:19 +0200

    0.1.0-feature/drop-gl-objects-inheritance.32+5 Implement dedicated VAO class

---
+++ app/src/main/java/io/jim/tesserapp/graphics/engine/GlVertexArray.kt
@@ -0,0 +1,23 @@
+package io.jim.tesserapp.graphics.engine
+
+import android.opengl.GLES30
+
+class GlVertexArray {
+
+    val handle = resultCode { GLES30.glGenVertexArrays(1, resultCode) }
+
+    /**
+     * Call [f] while the VAO is bound.
+     */
+    inline fun bound(crossinline f: () -> Unit) {
+        if (0 != resultCode { GLES30.glGetIntegerv(GLES30.GL_VERTEX_ARRAY_BINDING, resultCode) })
+            throw RuntimeException("Cannot bind VAO, another one is currently bound")
+
+        GLES30.glBindVertexArray(handle)
+
+        f()
+
+        GLES30.glBindVertexArray(0)
+    }
+
+}
--- app/src/main/java/io/jim/tesserapp/graphics/engine/GlVertexBuffer.kt
+++ app/src/main/java/io/jim/tesserapp/graphics/engine/GlVertexBuffer.kt
@@ -13,7 +13,7 @@ class GlVertexBuffer(private val drawMode: Int) {
     /**
      * Store vertex attribute pointers.
      */
-    val vertexArray = resultCode { GLES30.glGenVertexArrays(1, resultCode) }
+    val vertexArray = GlVertexArray()
 
     val buffer = GlBuffer(GLES30.GL_ARRAY_BUFFER, GLES30.GL_STATIC_DRAW)
     
@@ -50,14 +50,11 @@ class GlVertexBuffer(private val drawMode: Int) {
      * Call [f] while the VBO and VAO are bound.
      */
     inline fun bound(crossinline f: () -> Unit) {
-        if (0 != resultCode { GLES30.glGetIntegerv(GLES30.GL_VERTEX_ARRAY_BINDING, resultCode) })
-            throw RuntimeException("Cannot bind VAO, another one is currently bound")
-
-        GLES30.glBindVertexArray(vertexArray)
-
-        buffer.bound(f)
-
-        GLES30.glBindVertexArray(0)
+        vertexArray.bound {
+            buffer.bound {
+                f()
+            }
+        }
     }
     
 }
