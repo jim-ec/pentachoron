baed88caf44088e1224eb3dd08f2fdcb51977fbe
Jim-Eckerlein <jim.eckerlein@gmail.com>
2018-06-23 19:16:29 +0200

    0.1.0-feature/purify-math-functions.37+20 Simplify look-at matrix

--- app/src/main/java/io/jim/tesserapp/math/matrix/LookAtMatrix.kt
+++ app/src/main/java/io/jim/tesserapp/math/matrix/LookAtMatrix.kt
@@ -7,25 +7,21 @@ import io.jim.tesserapp.math.vector.VectorN
  * @param distance Position of eye along the x-axis.
  * @param refUp Up direction of camera.
  */
-fun lookAt(distance: Double, refUp: VectorN): Matrix {
+fun lookAt(distance: Double, refUp: VectorN) = run {
     
     val forward = VectorN(1.0, 0.0, 0.0)
     val right = refUp.normalized() cross forward
     val up = forward cross right
     
-    val matrix = Matrix(4)
-    
-    matrix[0, 0] = right.x
-    matrix[0, 1] = right.y
-    matrix[0, 2] = right.z
-    
-    matrix[1, 0] = up.x
-    matrix[1, 1] = up.y
-    matrix[1, 2] = up.z
-    
-    matrix[2, 0] = forward.x
-    matrix[2, 1] = forward.y
-    matrix[2, 2] = forward.z
-    
-    return Matrix.translation(4, VectorN(-distance, 0.0, 0.0)) * matrix.transposed()
+    Matrix.translation(4, VectorN(-distance, 0.0, 0.0)) * Matrix(4, 4, mapOf(
+            0 to 0 to right.x,
+            0 to 1 to right.y,
+            0 to 2 to right.z,
+            1 to 0 to up.x,
+            1 to 1 to up.y,
+            1 to 2 to up.z,
+            2 to 0 to forward.x,
+            2 to 1 to forward.y,
+            2 to 2 to forward.z
+    )).transposed()
 }
