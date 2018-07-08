12188b7817d6074c70432bd28ec7f507c168a08c
Jim-Eckerlein <jim.eckerlein@gmail.com>
2018-07-08 21:20:34 +0200

    0.1.0-feature/port-to-cpp.109+5 Raise C++ standard to C++17

--- app/src/main/cpp/renderer.cpp
+++ app/src/main/cpp/renderer.cpp
@@ -1,5 +1,15 @@
 #include <jni.h>
 
+#include <vector>
+#include <tuple>
+#include <string>
+
+static void foo() {
+    std::vector<std::pair<std::string, int>> v{{{"Jim", 20}, {"Alena", 12}}};
+    
+    auto [a, b] = v[0];
+}
+
 extern "C" JNIEXPORT void JNICALL
 Java_io_jim_tesserapp_ui_view_Renderer_drawGeometry(
         JNIEnv *env,
