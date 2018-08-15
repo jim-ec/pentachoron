303ce38b260bdf14aa114ad3c32dbbb0ce05101b
jim-eckerlein <jim.eckerlein@gmail.com>
2018-08-15 19:40:23 +0200

    The graphics view resides in its own fragment

--- app/src/main/java/io/jim/tesserapp/ui/main/MainActivity.kt
+++ app/src/main/java/io/jim/tesserapp/ui/main/MainActivity.kt
@@ -9,14 +9,11 @@ package io.jim.tesserapp.ui.main
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
-import android.view.ScaleGestureDetector
 import android.widget.Scroller
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.almeros.android.multitouch.MoveGestureDetector
 import io.jim.tesserapp.R
-import io.jim.tesserapp.graphics.Renderer
 import io.jim.tesserapp.graphics.gl.Color
 import io.jim.tesserapp.math.ScrollAttractor
 import io.jim.tesserapp.ui.preferences.gridPreference
@@ -197,94 +194,6 @@ class MainActivity : AppCompatActivity() {
             }
             
         }
-        
-        with(viewModel) {
-            listOf(
-                    translationX,
-                    translationY,
-                    translationZ,
-                    translationQ,
-                    rotationX,
-                    rotationY,
-                    rotationZ,
-                    rotationQ
-            )
-        }.forEach { transformLiveData ->
-            transformLiveData.observe(this, Observer {
-                glSurfaceView.invalidate()
-            })
-        }
-        
-        val orbitScrollAttractor = ScrollAttractor(40L)
-        val orbitScrollAttractorDeltanizerX = FloatDeltanizer(0f)
-        val orbitScrollAttractorDeltanizerY = FloatDeltanizer(0f)
-        
-        val orbitGestureDetector = MoveGestureDetector(this,
-                object : MoveGestureDetector.SimpleOnMoveGestureListener() {
-                    override fun onMove(detector: MoveGestureDetector?) = consume {
-                        detector ?: return NOT_CONSUMED
-                        orbitScrollAttractor.scrollTo(detector.focusX, detector.focusY)
-                    }
-                })
-        
-        val zoomGestureDetector = ScaleGestureDetector(this,
-                object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-                    override fun onScale(detector: ScaleGestureDetector?) = consume {
-                        detector ?: return NOT_CONSUMED
-                        
-                        // A scale factor > 1 means that the pointer distance increased, i.e. zooming out:
-                        viewModel.cameraDistance.value /= detector.scaleFactor
-                        
-                        // Constrain camera distance to a certain minimum:
-                        viewModel.cameraDistance.value = Math.max(viewModel.cameraDistance.value, 2.0)
-                    }
-                })
-        
-        glSurfaceView.apply {
-            setEGLContextClientVersion(2)
-            setRenderer(Renderer(
-                    Color(context.themedColorInt(R.attr.colorPrimary)),
-                    viewModel,
-                    context.assets,
-                    resources.displayMetrics.xdpi.toDouble()))
-            
-            setOnTouchListener { _, event ->
-                consume {
-                    zoomGestureDetector.onTouchEvent(event)
-                    orbitGestureDetector.onTouchEvent(event)
-                }
-            }
-            
-            viewTreeObserver.addOnPreDrawListener {
-                consume {
-                    synchronized(viewModel) {
-                        viewModel.horizontalCameraRotation.value += orbitScrollAttractorDeltanizerX.delta / 300.0
-                        viewModel.verticalCameraRotation.value -= orbitScrollAttractorDeltanizerY.delta / 300.0
-                        
-                        orbitScrollAttractor.computeNextApproximation().also {
-                            orbitScrollAttractorDeltanizerX.new = it.x
-                            orbitScrollAttractorDeltanizerY.new = it.y
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    /**
-     * Pause render-thread.
-     */
-    override fun onStop() {
-        super.onStop()
-        glSurfaceView.onPause()
-    }
-    
-    /**
-     * Resume render-thread.
-     */
-    override fun onResume() {
-        super.onResume()
-        glSurfaceView.onResume()
     }
     
 }
