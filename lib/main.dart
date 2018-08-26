97fdf4e9553967e1e6ebb2f69d5fe6d4542d248c
jim-eckerlein <jim.eckerlein@gmail.com>
2018-08-26 17:29:13 +0200

    Move app class into main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:tesserapp/tesserapp.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tesserapp/home.dart';
+import 'package:tesserapp/theme.dart';
 
 void main() => runApp(TesserApp());
+
+class TesserApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPaintSizeEnabled = false;
+    return MaterialApp(
+      title: "Tesserapp",
+      debugShowCheckedModeBanner: false,
+      theme: tesserTheme(),
+      home: Home(),
+    );
+  }
+}
