4faf21a4e65ffae5066a521c911315aeee933808
Jim Eckerlein <jim.eckerlein@gmail.com>
2018-09-03 21:51:04 +0200

    Fix backdrop back layer height on landscape orientation

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -82,100 +82,95 @@ class _BackdropState extends State<Backdrop>
 
   @override
   Widget build(BuildContext context) => LayoutBuilder(
-        builder: (context, constraints) {
-          final frontLayerTop = backBarHeight +
-              controller.value *
-                  (constraints.biggest.height -
-                      hiddenFrontLayerHeight -
-                      backBarHeight);
-          final backLayerHeight = constraints.biggest.height - backBarHeight;
-          return Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              // Background:
-              Container(
-                color: Theme.of(context).primaryColor,
-              ),
-              // Back Bar:
-              Positioned(
-                top: 0.0,
-                height: backBarHeight,
-                left: 0.0,
-                right: 0.0,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Center(
-                            child: Text(
-                              "TESSERAPP",
-                              style: Theme.of(context).primaryTextTheme.title,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned.fill(
-                        left: null,
-                        child: FlatButton(
-                          splashColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          onPressed: () {
-                            switch (controller.status) {
-                              case AnimationStatus.forward:
-                              case AnimationStatus.completed:
-                                controller.fling(velocity: -.01);
-                                break;
-                              case AnimationStatus.reverse:
-                              case AnimationStatus.dismissed:
-                                controller.fling(velocity: 0.01);
-                                break;
-                            }
-                          },
+        builder: (context, constraints) => Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                // Background:
+                Container(
+                  color: Theme.of(context).primaryColor,
+                ),
+                // Back Bar:
+                Positioned(
+                  top: 0.0,
+                  height: backBarHeight,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned.fill(
                           child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16.0),
-                            child: AspectRatio(
-                              aspectRatio: 1.0,
-                              child: MorphingArrow(
-                                color: Theme
-                                    .of(context)
-                                    .primaryTextTheme
-                                    .title
-                                    .color,
-                                advance: controller.value,
+                            padding: const EdgeInsets.all(16.0),
+                            child: Center(
+                              child: Text(
+                                "TESSERAPP",
+                                style: Theme.of(context).primaryTextTheme.title,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Positioned.fill(
+                          left: null,
+                          child: FlatButton(
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onPressed: () {
+                              switch (controller.status) {
+                                case AnimationStatus.forward:
+                                case AnimationStatus.completed:
+                                  controller.fling(velocity: -.01);
+                                  break;
+                                case AnimationStatus.reverse:
+                                case AnimationStatus.dismissed:
+                                  controller.fling(velocity: 0.01);
+                                  break;
+                              }
+                            },
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: AspectRatio(
+                                aspectRatio: 1.0,
+                                child: MorphingArrow(
+                                  color: Theme
+                                      .of(context)
+                                      .primaryTextTheme
+                                      .title
+                                      .color,
+                                  advance: controller.value,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              // Back layer:
-              Positioned(
-                top: backBarHeight,
-                height: backLayerHeight,
-                left: 0.0,
-                right: 0.0,
-                child: backLayer,
-              ),
-              // Front layer:
-              Positioned.fill(
-                top: frontLayerTop,
-                bottom: -frontLayerTop,
-                left: 0.0,
-                right: 0.0,
-                child: wrapInGestureDetectorIfClosed(constraints, frontLayer),
-              ),
-            ],
-          );
-        },
+                // Back layer:
+                Positioned(
+                  top: backBarHeight,
+                  bottom: hiddenFrontLayerHeight,
+                  left: 0.0,
+                  right: 0.0,
+                  child: backLayer,
+                ),
+                // Front layer:
+                Positioned(
+                  top: backBarHeight +
+                      controller.value *
+                          (constraints.biggest.height -
+                              hiddenFrontLayerHeight -
+                              backBarHeight),
+                  height: constraints.biggest.height,
+                  left: 0.0,
+                  right: 0.0,
+                  child: wrapInGestureDetectorIfClosed(constraints, frontLayer),
+                ),
+              ],
+            ),
       );
-
-  get s => 1.0 + controller.value * 0.4;
 
   @override
   void dispose() {
