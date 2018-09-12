667977815997ae7679202b38eec173863cb0e920
jim-eckerlein <jim.eckerlein@gmail.com>
2018-09-12 17:25:00 +0200

    Improve backdrop morphing arrow visual appearance

--- lib/backdrop/backdrop.dart
+++ lib/backdrop/backdrop.dart
@@ -1,6 +1,7 @@
 library backdrop;
 
 import 'package:flutter/material.dart';
+import 'package:tesserapp/generic/angle.dart';
 
 part 'morphing_arrow.dart';
 part 'widget.dart';
--- lib/backdrop/morphing_arrow.dart
+++ lib/backdrop/morphing_arrow.dart
@@ -22,32 +22,34 @@ class MorphingArrow extends StatelessWidget {
 @immutable
 class _Line {
   final Offset from, to;
+
   const _Line(this.from, this.to);
 }
 
 @immutable
 class _LinePoles {
   final _Line start, end;
+
   const _LinePoles(this.start, this.end);
 
   _Line lerp(final double advance) => _Line(
-    Offset.lerp(start.from, end.from, advance),
-    Offset.lerp(start.to, end.to, advance),
-  );
+        Offset.lerp(start.from, end.from, advance),
+        Offset.lerp(start.to, end.to, advance),
+      );
 }
 
 class _MorphingArrowPainter extends CustomPainter {
   final double advance;
   final Paint arrowPaint;
-  
+
   static const linePoles = [
     _LinePoles(
       _Line(Offset(0.3, 0.4), Offset(0.5, 0.6)),
-      _Line(Offset(0.3, 1.0 - 0.4), Offset(0.5, 1.0 - 0.6)),
+      _Line(Offset(0.7, 0.3), Offset(0.3, 0.7)),
     ),
     _LinePoles(
       _Line(Offset(0.5, 0.6), Offset(0.7, 0.4)),
-      _Line(Offset(0.5, 1.0 - 0.6), Offset(0.7, 1.0 - 0.4)),
+      _Line(Offset(0.7, 0.7), Offset(0.3, 0.3)),
     ),
   ];
 
@@ -62,12 +64,15 @@ class _MorphingArrowPainter extends CustomPainter {
       oldDelegate.advance != advance;
 
   @override
-  void paint(final Canvas canvas, final Size size) => linePoles
-          .map((linePoles) => linePoles.lerp(advance))
-          .expand((line) => [line.from, line.to])
-          .map((point) => point.scale(size.width, size.height))
-          .reduce((from, to) {
-        canvas.drawLine(from, to, arrowPaint);
-        return to;
-      });
+  void paint(final Canvas canvas, final Size size) {
+    canvas.translate(size.width / 2, size.height / 2);
+    canvas.rotate(Angle.fromPi(advance).radians);
+    canvas.translate(-size.width / 2, -size.height / 2);
+    linePoles
+      .map((linePoles) => linePoles.lerp(advance))
+      .forEach((line) => canvas.drawLine(
+          line.from.scale(size.width, size.height),
+          line.to.scale(size.width, size.height),
+          arrowPaint));
+  }
 }
