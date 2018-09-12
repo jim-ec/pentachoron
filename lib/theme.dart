068ceedff0548d24cb5700582b7faf528dcc539a
jim-eckerlein <jim.eckerlein@gmail.com>
2018-09-12 19:25:18 +0200

    Switch to lime colored theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,21 +2,12 @@ import 'package:flutter/material.dart';
 
 ThemeData tesserTheme(final Brightness brightness) => ThemeData(
       primaryColor: (brightness == Brightness.light)
-          ? Colors.deepOrangeAccent[400]
+          ? Colors.lime[800]
           : Colors.grey[900],
-      toggleableActiveColor: Colors.deepOrangeAccent[700],
-      accentColor: Colors.deepOrangeAccent[700],
+      toggleableActiveColor: Colors.lime[900],
+      accentColor: Colors.lime[800],
       fontFamily: "GeoNMS",
       brightness: brightness,
-      textTheme: TextTheme(
-        body1: TextStyle(
-          letterSpacing: 0.8,
-        ),
-        button: TextStyle(
-          fontWeight: FontWeight.w900,
-          letterSpacing: 1.2,
-        ),
-      ),
       primaryTextTheme: TextTheme(
         title: TextStyle(
           fontSize: 24.0,
@@ -24,6 +15,7 @@ ThemeData tesserTheme(final Brightness brightness) => ThemeData(
         ),
         body1: TextStyle(
           letterSpacing: 0.8,
+          fontWeight: FontWeight.w600,
         ),
         button: TextStyle(
           fontWeight: FontWeight.w900,
