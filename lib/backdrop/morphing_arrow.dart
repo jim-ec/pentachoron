3e95a62794f5afa297b081d0700b5c31fff4bc3b
jim-eckerlein <jim.eckerlein@gmail.com>
2018-09-12 17:09:24 +0200

    Improve backdrop morphing arrow implementation

--- lib/backdrop/morphing_arrow.dart
+++ lib/backdrop/morphing_arrow.dart
@@ -19,38 +19,52 @@ class MorphingArrow extends StatelessWidget {
       );
 }
 
+@immutable
+class _Line {
+  final Offset from, to;
+  const _Line(this.from, this.to);
+}
+
+@immutable
+class _LinePoles {
+  final _Line start, end;
+  const _LinePoles(this.start, this.end);
+
+  _Line lerp(final double advance) => _Line(
+    Offset.lerp(start.from, end.from, advance),
+    Offset.lerp(start.to, end.to, advance),
+  );
+}
+
 class _MorphingArrowPainter extends CustomPainter {
   final double advance;
   final Paint arrowPaint;
-
-  /// Three points defining the arrow in a unit space.
-  static const points = <Offset>[
-    Offset(0.3, 0.4),
-    Offset(0.5, 0.6),
-    Offset(0.7, 0.4),
+  
+  static const linePoles = [
+    _LinePoles(
+      _Line(Offset(0.3, 0.4), Offset(0.5, 0.6)),
+      _Line(Offset(0.3, 1.0 - 0.4), Offset(0.5, 1.0 - 0.6)),
+    ),
+    _LinePoles(
+      _Line(Offset(0.5, 0.6), Offset(0.7, 0.4)),
+      _Line(Offset(0.5, 1.0 - 0.6), Offset(0.7, 1.0 - 0.4)),
+    ),
   ];
 
   _MorphingArrowPainter(this.advance, final Color color)
       : arrowPaint = Paint()
           ..color = color
           ..strokeWidth = 2.0
-          ..strokeCap = StrokeCap.round
-          ..strokeJoin = StrokeJoin.round;
-
-  /// Linearly interpolation between the actual arrow shape as given by
-  /// [points] and its vertically mirrored version.
-  /// This effectively morphs between the default down arrow and the up
-  /// arrow.
-  Offset lerpPoint(final Offset point) =>
-      Offset.lerp(point, Offset(point.dx, 1.0 - point.dy), advance);
+          ..strokeCap = StrokeCap.round;
 
   @override
   bool shouldRepaint(final _MorphingArrowPainter oldDelegate) =>
       oldDelegate.advance != advance;
 
   @override
-  void paint(final Canvas canvas, final Size size) => points
-          .map(lerpPoint)
+  void paint(final Canvas canvas, final Size size) => linePoles
+          .map((linePoles) => linePoles.lerp(advance))
+          .expand((line) => [line.from, line.to])
           .map((point) => point.scale(size.width, size.height))
           .reduce((from, to) {
         canvas.drawLine(from, to, arrowPaint);
