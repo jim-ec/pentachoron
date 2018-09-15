8cb225c76111627612314d37904c0ca783cf5dc4
Jim Eckerlein <jim.eckerlein@gmail.com>
2018-09-15 13:09:21 +0200

    Extend angle class

--- lib/generic/angle.dart
+++ lib/generic/angle.dart
@@ -6,17 +6,32 @@ import 'package:meta/meta.dart';
 class Angle implements Comparable<Angle> {
   final double _storage;
 
+  /// Create a zero angle.
   const Angle.zero() : _storage = 0.0;
 
+  /// Create an angle defined by degrees.
+  /// One full turn equals 360 degrees.
   const Angle.fromDegrees(final double degrees)
       : _storage = degrees / 180.0 * math.pi;
 
+  /// Create an angle from radians.
+  /// One full turn equals 2pi radians.
   const Angle.fromRadians(final double radians) : _storage = radians;
 
+  /// Create an angle from radians over pi.
+  /// One full turn equals 2 pi.
   const Angle.fromPi(final double pi) : _storage = pi * math.pi;
 
+  /// Create an angle defined by gradians.
+  /// One full turn equals 400 gradians.
   const Angle.fromGradians(final double gradians)
       : _storage = gradians / 200.0 * math.pi;
+
+  /// Create an angle defined by turns.
+  /// One full turn equals 1 turn.
+  const Angle.fromTurns(final double turns) : _storage = turns * 2.0 * math.pi;
+
+  double get turns => (_storage) / math.pi / 2.0;
 
   double get degrees => (_storage / math.pi) * 180.0;
 
